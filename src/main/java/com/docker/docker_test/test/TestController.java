048d6832b467f3350c38146d498412d69df43b39
zpahf2018 <zpahf2018@naver.com>
2024-07-10 23:12:20 +0900

    깃 테스트 2

--- src/main/java/com/docker/docker_test/test/TestController.java
+++ src/main/java/com/docker/docker_test/test/TestController.java
@@ -1,5 +1,6 @@
 package com.docker.docker_test.test;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
